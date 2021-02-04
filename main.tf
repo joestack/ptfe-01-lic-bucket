bc9bbdd2692c6aa06fd81e245335e960a4e62f47
joestack <joern@joern-online.de>
2021-02-04 13:01:54 +0100

    del policy arn from kms

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "template_file" "tfe_s3_bootstrap_bucket_policy" {
 resource "aws_kms_key" "tfe_key" {
   description             = "AWS KMS Customer-managed key to encrypt TFE and other resources"
   key_usage               = "ENCRYPT_DECRYPT"
-  policy                  = data.template_file.kms_key_policy.rendered # was comment
+  #policy                  = data.template_file.kms_key_policy.rendered 
   deletion_window_in_days = 7
   is_enabled              = true
   enable_key_rotation     = false
